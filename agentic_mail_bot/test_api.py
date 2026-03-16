9441d699433b90dec9ad71cad05e5bb1407f39d0
fariz <178866636+mohammed-fariz@users.noreply.github.com>
2026-03-16 10:59:34 +0530

    ci pipeline

--- agentic_mail_bot/test_api.py
+++ agentic_mail_bot/test_api.py
@@ -1,6 +1,6 @@
 import pytest
 from fastapi.testclient import TestClient
-from agentic_mail_bot.backend.app import app,detect_intent
+from backend.app import app, detect_intent
 
 client = TestClient(app)
 
