cd061dbd23248696a3d5e5d3aec61d7445723dde
fariz <178866636+mohammed-fariz@users.noreply.github.com>
2026-03-16 11:02:34 +0530

    fixing test_api

--- agentic_mail_bot/test_api.py
+++ agentic_mail_bot/test_api.py
@@ -5,32 +5,19 @@ from backend.app import app, detect_intent
 client = TestClient(app)
 
 
-# --------------------------------------------------
-# TEST FASTAPI ROOT
-# --------------------------------------------------
 def test_home():
     response = client.get("/")
     assert response.status_code == 200
 
 
-# --------------------------------------------------
-# TEST INTENT DETECTION
-# --------------------------------------------------
 def test_detect_email_intent():
     assert detect_intent("send email to john") == "email"
 
 
 def test_detect_add_intent():
-    assert detect_intent("add 2 and 3") == "add"
+    assert detect_intent("add 5 and 3") == "add"
 
 
-def test_detect_search_intent():
-    assert detect_intent("who is elon musk") == "search"
-
-
-# --------------------------------------------------
-# TEST CHAT API
-# --------------------------------------------------
 def test_chat_endpoint():
     response = client.post(
         "/chat",
